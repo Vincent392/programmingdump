184ae138154e1c71be4c26ac1cb2eb5682371b9a
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-03-10 15:27:06 +0000

    Update

---
+++ compile.bat
@@ -0,0 +1,9 @@
+@Echo off
+Title PROGRAMMING DUMP COMPILE SCRIPT
+Echo Programming dump compile script
+Echo 2024 Vincent C
+Echo Compile Java program to jar (1/whoknows)
+javac ./java/ .java
+jar cvfe javadump.jar mainjava *.class
+Echo Compile Java program done!
+Echo Compile C Program (2/whoknows)
